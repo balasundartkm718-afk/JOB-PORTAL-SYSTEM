d97adf5c198d35fb7b3174c3fd91fcdcd47d2e15
balasundartkm718-afk <balasundartkm718@gmail.com>
2026-04-17 15:35:19 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GithubAuthProvider, signInWithPopup } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your config (same as yours)
 const firebaseConfig = {
   apiKey: "AIzaSyC1k1wds6P39Bbc51o6uo1JWgLOCEaf5Hg",
   authDomain: "jpms-dc2dc.firebaseapp.com",
   projectId: "jpms-dc2dc",
   storageBucket: "jpms-dc2dc.firebasestorage.app",
   messagingSenderId: "1089495911873",
-  appId: "1:1089495911873:web:6b5044f0cfa2e3bd2c7340",
-  measurementId: "G-JD00431Q73"
+  appId: "1:1089495911873:web:6b5044f0cfa2e3bd2c7340"
 };
 
-// Initialize Firebase
+// Initialize
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
