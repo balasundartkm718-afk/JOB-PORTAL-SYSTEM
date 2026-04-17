4605835d15592c4c3724475faab8534b496fb40d
balasundartkm718-afk <balasundartkm718@gmail.com>
2026-04-17 16:11:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,23 @@
-// ============================================================
-// firebase-config.js — NexHire Job Portal
-// ============================================================
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyC1k1wds6P39Bbc51o6uo1JWgLOCEaf5Hg",
-  authDomain: "jpms-dc2dc.firebaseapp.com",
-  projectId: "jpms-dc2dc",
-  storageBucket: "jpms-dc2dc.firebasestorage.app",
-  messagingSenderId: "1089495911873",
-  appId: "1:1089495911873:web:6b5044f0cfa2e3bd2c7340",
-  measurementId: "G-JD00431Q73"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC1k1wds6P39Bbc51o6uo1JWgLOCEaf5Hg",
+    authDomain: "jpms-dc2dc.firebaseapp.com",
+    projectId: "jpms-dc2dc",
+    storageBucket: "jpms-dc2dc.firebasestorage.app",
+    messagingSenderId: "1089495911873",
+    appId: "1:1089495911873:web:6b5044f0cfa2e3bd2c7340",
+    measurementId: "G-JD00431Q73"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
