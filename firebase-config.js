27f2c35fea8d7eba753a2f9f56f6e0ca933f8692
balasundartkm718-afk <balasundartkm718@gmail.com>
2026-04-17 16:23:08 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,4 @@
-// firebase.js
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyC1k1wds6P39Bbc51o6uo1JWgLOCEaf5Hg",
   authDomain: "jpms-dc2dc.firebaseapp.com",
   projectId: "jpms-dc2dc",
@@ -11,8 +7,3 @@ const firebaseConfig = {
   appId: "1:1089495911873:web:6b5044f0cfa2e3bd2c7340",
   measurementId: "G-JD00431Q73"
 };
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export { app, analytics };
